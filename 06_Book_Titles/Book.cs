77031365b88bb83f4042b24b83762b27217e5002
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2016-09-19 11:01:46 +0200

    Wrote my own capitalize

--- 06_Book_Titles/Book.cs
+++ 06_Book_Titles/Book.cs
@@ -1,76 +1,52 @@
-﻿
-using System.Globalization;
-using System.Threading;
-using System.Collections.Generic;
+﻿namespace _06_Book_Titles{
 
-namespace _06_Book_Titles
-{
+    internal class Book{
 
-    internal class Book
-    {
         internal string Titleized;
-        public Book()
-        {
-            
-        }
-        public string Title
-        {
-            get
-            {
+        public string Title{
+
+            get{
                 return Titleized;
             }
-            internal set
-            {
+            internal set{
                 Titleized = value;
                 Titleized = Titleize();
             }
         }
-        public string Titleize()
-        {
-            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
-            TextInfo textInfo = cultureInfo.TextInfo;
+        public string Titleize(){
+
             string[] cap;
-            List<string> sreturn = new List<string>();
-            char[] space = { ' ' };
-            cap = Title.Split(space);
+            string finalWord = "";
+            cap = Title.Split(' ');
             string[] nonCap = { "and", "over", "the", "in", "of", "an", "a" };
             bool firstRun = true;
-            int i = 0;
-            foreach (string word in cap)
-            {
-                if (!Contains(word, nonCap) || firstRun)
-                {
+            foreach (string word in cap){
+                if (!Contains(word, nonCap) || firstRun){
                     firstRun = false;
-                    sreturn.Add(textInfo.ToTitleCase(word));
-                    i++;
+                    char firstLetter = word[0];
+                    string restWord = word.Substring(1);
+                    finalWord += char.ToUpper(firstLetter) + restWord + " ";
                 }
-                else
-                {
-                    sreturn.Add(word);
-                    i++;
+                else{
+                    finalWord += word + " ";
                 }
 
             }
-            string[] areturn = sreturn.ToArray();
-            return string.Join(" ", areturn);
+            return finalWord.Trim(' ');
         }
-        internal bool Contains(string v, string[] x)
-        {
+        internal bool Contains(string v, string[] x){
+
             bool result;
             int a = 0;
-            for (int i = 0; i < x.Length; i++)
-            {
-                if (v == x[i])
-                {
+            for (int i = 0; i < x.Length; i++){
+                if (v == x[i]){
                     a++;
                 }
             }
-            if (a > 0)
-            {
+            if (a > 0){
                 result = true;
             }
-            else
-            {
+            else{
                 result = false;
             }
             return result;
